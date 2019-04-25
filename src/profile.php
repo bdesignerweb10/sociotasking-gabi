acda2d0197f0ed9587834136cbc6f824a4b5b6ed
Bruno Gomes <bdesigner_web@hotmail.com>
2019-04-25 14:46:19 -0300

    Ajustes responsividade

--- src/profile.php
+++ src/profile.php
@@ -1,18 +1,31 @@
 <?php
 	require_once('header.php');
-?>
+?> 
 <main class="profile-page">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-4">
 				<img src="img/gabi-profile.jpg">
+				<p class="social">Me acompanhe nas redes sociais</p>
+				<p>
+					<a href="https://www.facebook.com/Gabi.Viero" target="_blank"><i class="fab fa-facebook-square fa-2x"></i></a> 
+					<a href="https://www.instagram.com/gabi_viero/?hl=pt-br" target="_blank"><i class="fab fa-instagram fa-2x"></i></a>
+				</p>
 			</div><!-- col-sm-4 -->
 			<div class="col-sm-8">
 				<h3 class="headline-profile"><span>Gabrielle Viero</span></h3>
+				<p>Um dia me disseram que empreender era a maior loucura que eu poderia fazer na minha vida, mas ironicamente eu descobri na prática que quem me disse isso estava totalmente equivocado. Meu nome é Gabrielle Viero e eu quero compartilhar com você a minha descoberta.</p>
+				<p>Tenho 25 anos, e a 2 anos e meio fui agraciada com uma oportunidade chamada EMPREENDEDORISMO, e foi amor a primeira vista. Tudo começou com uma trágica demissão, eu fiquei no fundo do poço, me sentindo a pior pessoa do mundo principalmente por ter que voltar para o mercado de trabalho novamente e começar tudo do zero em uma nova empresa.</p>
+				<p>E foi nesse período que uma pessoa muito relevante em minha vida, a Érika Castro, me apresentou um divisor de águas no sentido de sonhos e realização profissional, nascia ali a oportunidade de trabalhar na internet . Fiquei cara a cara com a plataforma mais completa do mercado de marketing digital a Builderall, e desde então não saí mais desse mundo motivador. Comecei de forma bem despretenciosa, e juntamente com a Érika (uma irmã praticamente, amiga mesmo, pessoa que estimo e admiro demais), demos forma a uma agência de marketing digital, a NewUp, que em 5 meses de atividade já tinha uma carteira de clientes com empresas conceituadas da cidade de Bauru/Sp, sim sou bauruense com muito amor.</p><p>E o que começou tímido e despretencioso foi tomando proporções cada vez maiores e desafiadoras e com muito trabalho, disciplina e estudos descobri que o meu propósito está ligado ao marketing de conteúdo.</p><p>Atualmente continuo trabalhando com a agência e aderindo a vários projetos promissores e a Socio Tasking é um desses projetos, atualmente a menina dos meus olhos, pois é uma oportunidade única de transformação de vida.</p>
 			</div><!-- col-sm-8 -->
+			<div class="col-sm-12 actions">
+				<a href="http://bit.ly/GABRIELLEVIERO" target="_blank"><button type="button" class="btn btn-lg btn-whatsapp"><i class="fab fa-whatsapp"></i> Estou à disposição</button></a> 
+				<a href="https://sociotasking.com/?invite=gabrielle"><button type="button" class="btn btn-lg btn-register">Cadastrar na Sócio Tasking</button></a> 
+				<a href="https://sociotasking.com/aovivo?invite=gabrielle"><button type="button" class="btn btn-lg btn-web">Assistir Webinar às 20h</button></a>
+			</div><!-- col-sm-12-->
 		</div><!-- row -->
 	</div><!-- container -->
-</main>
+</main> 
 <?php
-	require_once('footer.php');
+require_once('footer.php');
 ?>
